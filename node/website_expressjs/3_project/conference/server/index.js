a71cb34706e142a9ac9e1ff2aab041ad369eb3b0
ahmedibrahimq <ahmedimiq@gmail.com>
2019-12-28 14:26:42 +0200

    configuring pug

--- node/website_expressjs/3_project/conference/server/index.js
+++ node/website_expressjs/3_project/conference/server/index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
+
+// Setting views configs
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "./views"));
+if (app.get('env' === 'development')) {
+    app.locals.pretty = true; // Do not optimize rendered files
+}
+
+// Static files
 app.use(express.static("public")); // a static middleware this path is relative to the application root folder
 app.get("/favicon.ico", (req, res, next) => res.sendStatus(204)); // empty responce:(No Content) Nothing to see here
+
+// The main route
 app.use("/", routes()); // a routing middleware that reacts to the '/' regardless of the request verb(get,post ...)
 
 app.listen(3000);
