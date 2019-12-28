614b8e90d7ed5589e2325371323386aa4fa62ee5
ahmedibrahimq <ahmedimiq@gmail.com>
2019-12-28 14:28:09 +0200

    render index.pug as the home page

--- node/website_expressjs/3_project/conference/server/routes/index.js
+++ node/website_expressjs/3_project/conference/server/routes/index.js
@@ -5,7 +5,7 @@ const feedbackRoute = require("./feedback");
 const router = express.Router();
 
 module.exports = () => {
-    router.get("/", (req, res, next) => res.send("Index"));
+    router.get("/", (req, res, next) => res.render("index"));
     router.use("/speakers", speakersRoute());
     router.use("/feedback", feedbackRoute());
     return router;
