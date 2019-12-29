2405eb2551de6be97d6a30cd5172d76c219ada9f
ahmedibrahimq <ahmedimiq@gmail.com>
2019-12-29 14:02:00 +0200

    Adding a middleware for handling errors. Handling unmatched routes using http-errors module

--- node/website_expressjs/3_project/conference/server/index.js
+++ node/website_expressjs/3_project/conference/server/index.js
@@ -1,5 +1,6 @@
 const {join} = require("path");
 const express = require("express");
+const httpErrors = require("http-errors")
 const routes = require("./routes");
 
 const app = express();
@@ -11,12 +12,34 @@ if (app.get('env') === 'development') {
     app.locals.pretty = true; // Do not optimize rendered files
 }
 
-// Static files
+// Middlewares
+
 app.use(express.static("public")); // a static middleware this path is relative to the application root folder
 app.get("/favicon.ico", (req, res, next) => res.sendStatus(204)); // empty responce:(No Content) Nothing to see here
 
+// Routes
+
 // The main route
 app.use("/", routes()); // a routing middleware that reacts to the '/' regardless of the request verb(get,post ...)
+
+//Handling 404 Unknown routes
+// This matches if no other route matches. This handler comes after all routes are processed, to be sure that no other route matched
+app.use((req, res, next) => {
+    return next(httpErrors(404, "Page not found"));
+});
+
+// Errors handler (middleware)
+app.use((err, req, res, next) => {
+    const errStatus = err.status || 500; // 500 to cover the case when there is a server error that we don't know about
+    
+    // Store error info as properties in the response locals
+    res.locals.msg = err.message;
+    res.locals.status = errStatus;
+    res.locals.err = req.app.get("env") === "development" ? err : {}; // Show the stack of error
+    
+    res.status(errStatus); // set the response status
+    return res.render("error");
+});
 
 app.listen(3000);
 
