3cb246478a9a665766dc9b4a0e0103f6d7943d51
ahmedibrahimq <ahmedimiq@gmail.com>
2019-12-29 14:05:55 +0200

    Implementing site structure with pug

--- node/website_expressjs/3_project/conference/server/routes/speakers/index.js
+++ node/website_expressjs/3_project/conference/server/routes/speakers/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = () => {
-    router.get("/", (req, res, next) => res.send("All Speakers"));
-    router.get("/:name", (req, res, next) => res.send(`Speaker: ${req.params.name}`));
+    router.get("/", (req, res, next) => res.render("speakers"));
+    router.get("/:name", (req, res, next) => res.render("speakers/detail"));
     return router
 };
