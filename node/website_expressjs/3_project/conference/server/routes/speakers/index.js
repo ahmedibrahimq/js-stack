0a11da019a9fbf6e18e0096b9a2702e1b8525d24
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-01 22:12:56 +0200

    Providing data to speakers view and speaker detail view

--- node/website_expressjs/3_project/conference/server/routes/speakers/index.js
+++ node/website_expressjs/3_project/conference/server/routes/speakers/index.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = () => {
-    router.get("/", (req, res, next) => res.render("speakers",  {pgName: "Home"}));
-    router.get("/:name", (req, res, next) => res.render("speakers/detail",  {pgName: req.params.name}));
+module.exports = (args = {}) => {
+    const { speakerService } = args;
+
+    router.get("/", async (req, res, next) => {
+        try {
+            const promises = [];
+            promises.push(speakerService.getNamesTitlesSummary());
+            promises.push(speakerService.getAllArtwork());
+            [speakersListwithSummary, artwork] = await Promise.all(promises);
+
+            return res.render("speakers", {
+                pgName: "Speakers",
+                speakersListwithSummary,
+                artwork,
+            });
+        } catch (err) { next(err); }
+
+    });
+
+    router.get("/:name", async (req, res, next) => {
+        try {
+            const speaker = await speakerService.getSpeakerByShortname(req.params.name);
+            if(!speaker) {
+                return next();
+            }
+            return res.render("speakers/detail", { pgName: req.params.name,  speaker, artwork: speaker.artwork})
+        } catch (err) { next(err); }
+    });
     return router
 };
