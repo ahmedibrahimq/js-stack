9c42448e6e60f4dfd3a33a2d665983cf4aeba39b
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-06 18:25:22 +0200

    Using bcrypt module to hash and validate user passwords

--- node/expressjs/advanced-project/server/models/UserModel.js
+++ node/expressjs/advanced-project/server/models/UserModel.js
@@ -8,7 +8,9 @@
 
 const mongoose = require('mongoose');
 const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
 
+const SALT_ROUNDS = 12;
 const UserSchema = mongoose.Schema({
     username: {
         type: String,
@@ -38,5 +40,32 @@ const UserSchema = mongoose.Schema({
     // a configuration option that makes mongoose create and add an updated time stamp for each document
     timestamps: true,
 });
+
+/**
+ * pre 'save' -> a function that will run every save for document
+ * NOTIC: we can't use arrow function in the callback because we want to use `this` keyword
+ */
+UserSchema.pre('save', async function preSave(next) {
+    const user = this;
+    const passwordModified = user.isModified('password');
+    if (!passwordModified) { return next() }
+    
+    try {
+        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+        user.password = hash;
+        return next();
+    } catch (err) {
+        return next(err)
+    }
+});
+
+/**
+ * Mongoose way to create instance method:
+ *  a method that will be available on every document that comes from the database.
+ */
+UserSchema.methods.comparePassword= async function comparePassword(candidate) {
+    // compares in a linear time
+    return bcrypt.compare(candidate, this.password);
+}
 
 module.exports = mongoose.model('User', UserSchema);
