a17d44c82df66b781a09127818cfc63f0f3fac4a
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-06 15:59:27 +0200

    Create a schema of users with mongoose

---
+++ node/expressjs/advanced-project/server/models/UserModel.js
@@ -0,0 +1,36 @@
+/**
+ * Mongoose uses schemas to define the structure and data types within a document.
+ *  To create and manipulate data within a defined schema,
+ *  Mongoose gives us a the concept of models.
+ * A model is the programming interface that lets you create and manipulate data
+ *  based on the schema.
+ */
+
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        index: { unique: true },
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        index: { unique: true },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+    },
+}, {
+    // a configuration option that makes mongoose create and add an updated time stamp for each document
+    timestamps: true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
