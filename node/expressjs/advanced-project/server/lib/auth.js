076543eb6a20a534f36903c4ae6cc5888e249773
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-07 19:31:44 +0200

    Setting up local authentication strategy against database for Passport using passport-local module

--- node/expressjs/advanced-project/server/lib/auth.js
+++ node/expressjs/advanced-project/server/lib/auth.js
@@ -1,4 +1,32 @@
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/UserModel');
+
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    async (username, password, callback) => {
+        try {
+            const user = UserModel.findOne({ email: username }).exec(); // exec to get a real promise back
+            const noError = null;
+            const userFound = true;
+            
+            if (!user) {
+                return callback(noError, !userFound, { message: 'Invalid email or password' })
+            }
+
+            const passwordOk = await user.comparePassword(password);
+            if (!passwordOk) {
+                return callback(noError, !userFound, { message: 'Invalid email or password' })
+            }
+
+            // a valid user with a valid password
+            return callback(noError, user);
+
+        } catch (err) {
+            return callback(err)
+        }
+    }
+));
 
 module.exports = {
     initialize: passport.initialize(),
