236a94121f2af297aaf1adc21590fdebc92cbaf2
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-08 21:58:06 +0200

    Adding a middleware to handle avatar

--- node/expressjs/advanced-project/server/routes/middlewares.js
+++ node/expressjs/advanced-project/server/routes/middlewares.js
@@ -14,3 +14,21 @@ const upload = multer({
 });
 
 module.exports.upload = upload;
+
+module.exports.handleAvatars = avatars => async (req, res, next) => {
+    if (!req.file) {
+        return next()
+    }
+    if (req.file.mimetype !== 'image/png' && req.file.mimetype !== 'image/jpeg') {
+        return next(new Error('File format is not supported'))
+    }
+
+    // a valid image
+    /**
+     * A very common pattern to use:
+     * If you pass information down to other middlewares, 
+     *  just add it to the request object.
+     */
+    req.file.storedFilename = await avatars.store(req.file.buffer);
+    return next();
+};
