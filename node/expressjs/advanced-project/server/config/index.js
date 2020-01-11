b13b0a43aaa6739f2ed7043d4464fc2de01abd67
ahmedibrahimq <ahmedimiq@gmail.com>
2020-01-11 09:51:22 +0200

    Configuring bunyan: adding log levels for each environment

--- node/expressjs/advanced-project/server/config/index.js
+++ node/expressjs/advanced-project/server/config/index.js
@@ -1,10 +1,25 @@
 require('dotenv').config();
 
 const path = require('path');
+const bunyan = require('bunyan');
+
+/**
+ * make the logging use different log levels depending on the environment we are running in
+ * In config loggers should be a function, because we want to lazy load those loggers
+ *  and not instantiate all of them.
+ * log level in test: only show fatal errors because otherwise
+ *  it would just clutter testing results if there are many logs
+ */
+const loggers = {
+  development: () => bunyan.createLogger({ name: 'development', level: 'debug'}),
+  production: () => bunyan.createLogger({ name: 'production', level: 'info'}),
+  test: () => bunyan.createLogger({ name: 'test', level: 'fatal'}),
+}
 
 module.exports = {
   development: {
     sitename: 'Roux Meetups [Development]',
+    log: loggers.development,
     data: {
       speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback.json'),
@@ -16,6 +31,7 @@ module.exports = {
   },
   production: {
     sitename: 'Roux Meetups',
+    log: loggers.production,
     data: {
       speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback.json'),
@@ -27,6 +43,7 @@ module.exports = {
   },
   test: {
     sitename: 'Roux Meetups [Test]',
+    log: loggers.test,
     data: {
       speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback-test.json'),
